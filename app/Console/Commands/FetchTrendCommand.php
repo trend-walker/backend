68c78a4ee3cde0ad599158bb838dc1156059ca52
yh-coder <yhcoder@gmail.com>
2019-10-02 01:02:15 +0900

    [update] 上位トレンド 取得の際に解析

--- app/Console/Commands/FetchTrendCommand.php
+++ app/Console/Commands/FetchTrendCommand.php
@@ -2,10 +2,15 @@
 
 namespace App\Console\Commands;
 
+use \Throwable;
+
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Carbon;
+
+use App\Model\Trend;
 use App\Services\TaskService;
-use \Throwable;
+use App\Services\TrendService;
 
 class FetchTrendCommand extends Command
 {
@@ -29,15 +34,21 @@ class FetchTrendCommand extends Command
     protected $taskService;
 
     /**
+     * @var TrendService
+     */
+    protected $trendService;
+
+    /**
      * Create a new command instance.
      *
      * @param TaskService $trendService
      * @return void
      */
-    public function __construct(TaskService $taskService)
+    public function __construct(TaskService $taskService, TrendService $trendService)
     {
         parent::__construct();
         $this->taskService = $taskService;
+        $this->trendService = $trendService;
     }
 
     /**
@@ -47,14 +58,19 @@ class FetchTrendCommand extends Command
      */
     public function handle()
     {
+        $upperPart = [];
+
+        // トレンド定期取得
         Log::info('fetch trends start.');
         try {
             $connection = $this->taskService->getApiConnection();
 
+            // トレンドワード取得
             $content = $connection->get("trends/place", ['id' => 23424856]);
             $list = $this->taskService->saveTrendData($content[0]);
             Log::info('save trends.');
 
+            // トレンドワード検索
             foreach ($list as $id => $word) {
                 $content = $connection->get("search/tweets", [
                     'q' => $word,
@@ -65,11 +81,27 @@ class FetchTrendCommand extends Command
                     'count' => 100,
                 ]);
                 $this->taskService->saveTrendTweets($content, $id);
+
+                // トレンドワード上位のID記録
+                if (count($upperPart) < 10) {
+                    $upperPart[] = $id;
+                }
             }
             Log::info('fetch trends over.');
         } catch (Throwable $e) {
             Log::info('fetch failure.');
             Log::debug($e);
+        }
+
+        // トレンドワード上位の解析
+        if (!empty($upperPart)) {
+            Log::info('analyze top trends start.');
+            $date = Carbon::now()->format('Y-m-d');
+            $trends = Trend::whereIn('id', $upperPart)->get();
+            foreach ($trends as $trend) {
+                $this->trendService->analyseDailyTrendTweets($date, $trend->trend_word_id);
+            }
+            Log::info(sprintf('analyze top %d trends end.', count($upperPart)));
         }
     }
 }
--- app/Services/TrendService.php
+++ app/Services/TrendService.php
@@ -258,9 +258,9 @@ SQL;
       $dateTarget = Carbon::parse($date);
       $dateCreation = Carbon::createFromTimestamp(stat($cacheFile)['ctime']);
 
-      // 定期更新の5分後に合わせる
+      // 定期更新に合わせる
       $dateCreation->second = 0;
-      $dateCreation->minute = 5 + 15 * (int) ($dateCreation->minute / 15);
+      $dateCreation->minute = 15 * (int) ($dateCreation->minute / 15);
 
       // キャッシュ破棄条件 指定日から25時間以内 && 現在から15分以上経過
       if ($dateTarget->gt($dateCreation->copy()->subHours(25)) && $dateNow->gt($dateCreation->copy()->addMinutes(15))) {
